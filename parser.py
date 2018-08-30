c6b0db411498a4341385ff81681fd1d9362c740c
Jerry Zhou <xiz170@ucsd.edu>
2018-08-30 14:32:51 -0700

    Add files via upload

--- parser.py
+++ parser.py
@@ -1,65 +1,79 @@
-import pandas as pd
-import numpy as np
+import csv
 import io
+import json
+import sys
+import os.path
 from datetime import datetime
 
+if (len(sys.argv)!=4):
+    print ('python parser.py  <input_file1> <input_file2> <output_file>')
+    exit(1)    
+
+if not (os.path.exists(sys.argv[1])) or not (os.path.exists(sys.argv[2])):
+    print ('At least one file does not exist.')
+    exit(1)
+
 # read two files
-df1 = pd.read_csv('CancerG2P_29_8_2018.csv', sep=',')
-df2 = pd.read_csv('DDG2P_29_8_2018.csv', sep=',')
-frames = [df1, df2]
-df = pd.concat(frames)
+dt1 = list(csv.reader(open('CancerG2P_29_8_2018.csv')))
+dt2 = list(csv.reader(open('DDG2P_29_8_2018.csv')))
 
-# identify missing data
-df.replace('No gene mim', np.nan, inplace=True)
-df.replace('No disease mim', np.nan, inplace=True)
+# clean data
+props_names = dt1[0]
+dt1_header_removed = dt1[1:]
+dt2_header_removed = dt2[1:]
 
-# separate id
-df_hgnc_id = df.loc[:,["hgnc id"]] 
-hgnc_id = df_hgnc_id.values[:,0]
-del df['hgnc id']
+def cleanCSV(ls):
+    for row in ls:
+        if (row[1]=='No gene mim'): row[1]=''
+        if (row[3]=='No disease mim'): row[3]=''
+        
+cleanCSV(dt1_header_removed)
+cleanCSV(dt2_header_removed)
+
+dt = dt1_header_removed + dt2_header_removed
 
 # parse and ignore empty data
-dt = df.values
-props_names = list(df)
-nan_tests = pd.isnull(dt)
+from datetime import datetime
 
-result_dict={} # intermediate dict construct
+result_dict={} # intermediate dict construct, with unique ids
 
-for x in range(0, len(dt)):
-    dict_gene = {} # for each observation
+for x in range(len(dt)):  # of all observations
+    dict_gene = {} # each observation's storage to attach to the 'gene2phenotype' of its unique id in main dict
     
-    for y in range(0, len(dt[0])): # of all observations
-        if not (nan_tests[x,y]): # of not NaN properties
+    for y in range(len(dt[0])): # of all properties
+        if dt[x][y]!='': # of not empty properties
             if (y==2 or y==3):
                 # additional processsing for disease name and mim
                 if 'disease' not in dict_gene:
                     dict_gene['disease'] = {}
-                dict_gene['disease'][props_names[y]] = dt[x,y]
+                dict_gene['disease'][props_names[y]] = dt[x][y]
             elif (y==7 or y==8 or y==11): 
                 # additional processing for rgan specificity list , pre symbols, phenotypes
-                dict_gene[props_names[y]] = dt[x,y].split(';')
+                dict_gene[props_names[y]] = dt[x][y].split(';')
             elif (y==9): 
                 # additional processing for pmids (list of integers)
-                dict_gene[props_names[y]] = [int(x) for x in dt[x,y].split(';')]
+                dict_gene[props_names[y]] = [int(x) for x in dt[x][y].split(';')]
             elif (y==12):
-                dict_gene[props_names[y]] = datetime.strptime(dt[x,y], '%Y-%m-%d %H:%M:%S')
+                continue
+            elif (y==13):
+                dict_gene[props_names[y]] = datetime.strptime(dt[x][y], '%Y-%m-%d %H:%M:%S')
             else:
-                dict_gene[props_names[y]] = dt[x,y]
+                dict_gene[props_names[y]] = dt[x][y]
     
-    if hgnc_id[x] in result_dict:
-        list_gene = result_dict[hgnc_id[x]]['gene2phenotype']
+    if int(dt[x][12]) in result_dict:
+        list_gene = result_dict[int(dt[x][12])]['gene2phenotype']
         list_gene.append(dict_gene)
-        result_dict[hgnc_id[x]]['gene2phenotype'] = list_gene
+        result_dict[int(dt[x][12])]['gene2phenotype'] = list_gene
     else:
         dict_item = {
-            "_id": hgnc_id[x],
+            "_id": int(dt[x][12]),
              "gene2phenotype": [dict_gene]
         }        
-        result_dict[hgnc_id[x]] = dict_item
+        result_dict[int(dt[x][12])] = dict_item
         
 result_list = list(result_dict.values())
 
 # save to a file
-file = io.open("result_from_script.txt","w",encoding='utf8')
-file.write(str(result_list))
+file = io.open(sys.argv[3],"w",encoding='utf8')
+file.write(json.dumps(result_list, indent=4, sort_keys=True, default=str))
 file.close()

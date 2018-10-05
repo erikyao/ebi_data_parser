5d02326e64173e8b285c7757a8f9cde62a4ab90d
namespacestd0 <xiz170@ucsd.edu>
2018-10-05 11:15:37 -0700

    BioThings Plug-in Compatible

--- parser.py
+++ parser.py
@@ -5,82 +5,76 @@ import sys
 import os.path
 from datetime import datetime
 
-if (len(sys.argv)!=4):
-    print ('python parser.py  <input_file1> <input_file2> <output_file>')
-    exit(1)    
+def load_data(input_file):
+    with open(input_file) as in_f:
+    
+        # read all
+        dt = list(csv.reader(in_f))
 
-if not (os.path.exists(sys.argv[1])) or not (os.path.exists(sys.argv[2])):
-    print ('At least one file does not exist.')
-    exit(1)
+        # catagorization
+        header = dt[0]
+        content = dt[1:]
 
-f1_name = sys.argv[1]
-f2_name = sys.argv[2]
-f3_name = sys.argv[3]
-
-def load_data(f1_name, f2_name, f3_name):
-    # read two files
-    dt1 = list(csv.reader(open(f1_name)))
-    dt2 = list(csv.reader(open(f2_name)))
-
-    # clean data
-    props_names = dt1[0]
-    dt1_header_removed = dt1[1:]
-    dt2_header_removed = dt2[1:]
-
-    def cleanCSV(ls):
-        for row in ls:
+        # remove empty values
+        for row in content:
             if (row[1]=='No gene mim'): row[1]=''
             if (row[3]=='No disease mim'): row[3]=''
+                
+        result_dict={} # intermediate dict construct, with unique ids
+
+        for x in range(len(content)):  # of all observations
+            dict_gene = {} # each observation's storage to attach to the 'gene2phenotype' of its unique id in main dict
             
-    cleanCSV(dt1_header_removed)
-    cleanCSV(dt2_header_removed)
+            for y in range(len(content[0])): # of all properties
+                if content[x][y]!='': # of not empty properties
+                    if (y==2 or y==3):
+                        # additional processsing for disease name and mim
+                        if 'disease' not in dict_gene:
+                            dict_gene['disease'] = {}
+                        dict_gene['disease'][header[y]] = content[x][y]
+                    elif (y==7 or y==8 or y==11): 
+                        # additional processing for rgan specificity list , pre symbols, phenotypes
+                        dict_gene[header[y]] = content[x][y].split(';')
+                    elif (y==9): 
+                        # additional processing for pmids (list of integers)
+                        dict_gene[header[y]] = [int(x) for x in content[x][y].split(';')]
+                    elif (y==12):
+                        # ignore _id here
+                        continue
+                    elif (y==13):
+                        dict_gene[header[y]] = datetime.strptime(content[x][y], '%Y-%m-%d %H:%M:%S')
+                    else:
+                        dict_gene[header[y]] = content[x][y]
+            # _id exists
+            if int(content[x][12]) in result_dict:
+                list_gene = result_dict[int(content[x][12])]['gene2phenotype']
+                list_gene.append(dict_gene)
+                result_dict[int(content[x][12])]['gene2phenotype'] = list_gene
+            else:
+                dict_item = {
+                    "_id": int(content[x][12]),
+                    "gene2phenotype": [dict_gene]
+                }        
+                result_dict[int(content[x][12])] = dict_item
+                
+        return list(result_dict.values())
 
-    dt = dt1_header_removed + dt2_header_removed
+if __name__ == "__main__":
 
-    # parse and ignore empty data
-    from datetime import datetime
+    if (len(sys.argv)!=3):
+        print ('python parser.py  <input_file> <output_file>')
+        exit(1)    
 
-    result_dict={} # intermediate dict construct, with unique ids
+    if not (os.path.exists(sys.argv[1])):
+        print ('input file does not exist.')
+        exit(1)
 
-    for x in range(len(dt)):  # of all observations
-        dict_gene = {} # each observation's storage to attach to the 'gene2phenotype' of its unique id in main dict
-        
-        for y in range(len(dt[0])): # of all properties
-            if dt[x][y]!='': # of not empty properties
-                if (y==2 or y==3):
-                    # additional processsing for disease name and mim
-                    if 'disease' not in dict_gene:
-                        dict_gene['disease'] = {}
-                    dict_gene['disease'][props_names[y]] = dt[x][y]
-                elif (y==7 or y==8 or y==11): 
-                    # additional processing for rgan specificity list , pre symbols, phenotypes
-                    dict_gene[props_names[y]] = dt[x][y].split(';')
-                elif (y==9): 
-                    # additional processing for pmids (list of integers)
-                    dict_gene[props_names[y]] = [int(x) for x in dt[x][y].split(';')]
-                elif (y==12):
-                    continue
-                elif (y==13):
-                    dict_gene[props_names[y]] = datetime.strptime(dt[x][y], '%Y-%m-%d %H:%M:%S')
-                else:
-                    dict_gene[props_names[y]] = dt[x][y]
-        
-        if int(dt[x][12]) in result_dict:
-            list_gene = result_dict[int(dt[x][12])]['gene2phenotype']
-            list_gene.append(dict_gene)
-            result_dict[int(dt[x][12])]['gene2phenotype'] = list_gene
-        else:
-            dict_item = {
-                "_id": int(dt[x][12]),
-                "gene2phenotype": [dict_gene]
-            }        
-            result_dict[int(dt[x][12])] = dict_item
-            
-    result_list = list(result_dict.values())
+    f1_name = sys.argv[1]
+    f2_name = sys.argv[2]
+
+    result_list = load_data(f1_name)
 
     # save to a file
-    file = io.open(f3_name,"w",encoding='utf8')
+    file = io.open(f2_name, "w", encoding='utf8')
     file.write(json.dumps(result_list, indent=4, sort_keys=True, default=str))
     file.close()
-
-load_data(f1_name, f2_name, f3_name)

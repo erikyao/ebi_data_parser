d2eb670a1b78ef5e607c0604b1951adb0e7c1301
namespacestd0 <xiz170@ucsd.edu>
2018-10-18 15:50:44 -0700

    Add release date extraction from http header

--- version.py
+++ version.py
@@ -1,2 +1,21 @@
+import re
+from datetime import datetime
+
 def get_release(self):
-    return "0.0.1"
+
+    dates = []
+    date_pattern = re.compile(r'\d\d_\d\d_\d\d\d\d')
+
+    for url in self.__class__.SRC_URLS:
+        res = self.client.head(url, allow_redirects=True)
+        header = res.headers['Content-Disposition']
+        match = date_pattern.findall(header)
+        assert len(match) == 1, "Parser Date Extraction Error"
+        dates.append(datetime.strptime(match[0], '%d_%m_%Y').date())
+
+    if dates[0] < dates[1]:
+        latest_version = dates[1]
+    else:
+        latest_version = dates[0]
+
+    return latest_version.isoformat()

de4f0ac178bc2e7a666479639764c5e06500847b
namespacestd0 <xzhou@scripps.edu>
2018-11-07 04:00:57 +0000

    Update date parsing regex to support single digit date format

--- version.py
+++ version.py
@@ -4,12 +4,15 @@ from datetime import datetime
 def get_release(self):
 
     dates = []
-    date_pattern = re.compile(r'\d\d_\d\d_\d\d\d\d')
+    date_pattern = re.compile(r'\d{1,2}_\d{1,2}_\d{4}')
 
     for url in self.__class__.SRC_URLS:
         res = self.client.head(url, allow_redirects=True)
         header = res.headers['Content-Disposition']
+        print(header)
         match = date_pattern.findall(header)
+	# test
+        print(match)
         assert len(match) == 1, "Parser Date Extraction Error"
         dates.append(datetime.strptime(match[0], '%d_%m_%Y').date())
 

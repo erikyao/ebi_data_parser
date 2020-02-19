88d47a1d5f5e658a998a42e4da88e0a7ad91654e
Kevin Xin <kevinxin@scripps.edu>
2020-02-19 09:15:38 -0800

    Update parser.py

--- parser.py
+++ parser.py
@@ -57,7 +57,8 @@ def load_data(data_folder):
                 elif (y==12):
                     continue
                 elif (y==13):
-                    dict_gene[props_names[y]] = datetime.strptime(dt[x][y], '%Y-%m-%d %H:%M:%S').isoformat()
+                    if dt[x][y] != 'No date':
+                        dict_gene[props_names[y]] = datetime.strptime(dt[x][y], '%Y-%m-%d %H:%M:%S').isoformat()
                 else:
                     dict_gene[props_names[y]] = dt[x][y]
         

9d30ce68d62d73d19b2133e9e90c86248149c0df
Yao Yao <yaoy.cs@gmail.com>
2023-06-23 16:44:50 -0700

    format code

--- parser.py
+++ parser.py
@@ -5,12 +5,12 @@ import sys
 import os.path
 from datetime import datetime
 
-def load_data(data_folder):
 
+def load_data(data_folder):
     # read two files
-    input_file_1 = os.path.join(data_folder,"CancerG2P_19_2_2020.csv")
+    input_file_1 = os.path.join(data_folder, "CancerG2P_19_2_2020.csv")
     assert os.path.exists(input_file_1), "Can't find input file '%s'" % input_file_1
-    input_file_2 = os.path.join(data_folder,"DDG2P_19_2_2020.csv")
+    input_file_2 = os.path.join(data_folder, "DDG2P_19_2_2020.csv")
     assert os.path.exists(input_file_2), "Can't find input file '%s'" % input_file_2
     dt1 = list(csv.reader(open(input_file_1)))
     dt2 = list(csv.reader(open(input_file_2)))
@@ -26,9 +26,11 @@ def load_data(data_folder):
 
     def cleanCSV(ls):
         for row in ls:
-            if (row[1]=='No gene mim'): row[1]=''
-            if (row[3]=='No disease mim'): row[3]=''
-            
+            if (row[1] == 'No gene mim'):
+                row[1] = ''
+            if (row[3] == 'No disease mim'):
+                row[3] = ''
+
     cleanCSV(dt1_header_removed)
     cleanCSV(dt2_header_removed)
 
@@ -36,32 +38,32 @@ def load_data(data_folder):
 
     # parse and ignore empty data
 
-    result_dict={} # intermediate dict construct, with unique ids
+    result_dict = {}  # intermediate dict construct, with unique ids
 
     for x in range(len(dt)):  # of all observations
-        dict_gene = {} # each observation's storage to attach to the 'gene2phenotype' of its unique id in main dict
-        
-        for y in range(len(dt[0])): # of all properties
-            if dt[x][y]!='': # of not empty properties
-                if (y==2 or y==3):
+        dict_gene = {}  # each observation's storage to attach to the 'gene2phenotype' of its unique id in main dict
+
+        for y in range(len(dt[0])):  # of all properties
+            if dt[x][y] != '':  # of not empty properties
+                if (y == 2 or y == 3):
                     # additional processsing for disease name and mim
                     if 'disease' not in dict_gene:
                         dict_gene['disease'] = {}
                     dict_gene['disease'][props_names[y]] = dt[x][y]
-                elif (y==7 or y==8 or y==11): 
+                elif (y == 7 or y == 8 or y == 11): 
                     # additional processing for rgan specificity list , pre symbols, phenotypes
                     dict_gene[props_names[y]] = dt[x][y].split(';')
-                elif (y==9): 
+                elif (y == 9):
                     # additional processing for pmids (list of integers)
                     dict_gene[props_names[y]] = [int(x) for x in dt[x][y].split(';')]
-                elif (y==12):
+                elif (y == 12):
                     continue
-                elif (y==13):
+                elif (y == 13):
                     if dt[x][y] != 'No date':
                         dict_gene[props_names[y]] = datetime.strptime(dt[x][y], '%Y-%m-%d %H:%M:%S').isoformat()
                 else:
                     dict_gene[props_names[y]] = dt[x][y]
-        
+
         if int(dt[x][12]) in result_dict:
             list_gene = result_dict[int(dt[x][12])]['gene2phenotype']
             list_gene.append(dict_gene)
@@ -70,16 +72,17 @@ def load_data(data_folder):
             dict_item = {
                 "_id": dt[x][12],
                 "gene2phenotype": [dict_gene]
-            }        
+            }
             result_dict[int(dt[x][12])] = dict_item
-            
+
     for v in result_dict.values():
         yield v
 
+
 if __name__ == "__main__":
 
-    if (len(sys.argv)!=2):
-        print ('python parser.py <output_file>')
+    if (len(sys.argv) != 2):
+        print('python parser.py <output_file>')
         exit(1)
 
     result_list = []
